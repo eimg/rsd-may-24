99cc0ba035ce13e0de10c5c4a6d4803866183140
Ei Maung <eimg@fairwayweb.com>
2024-05-12 15:54:15 +0630

    react

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,25 +1,84 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import Item from "./Item";
 
 export default function App() {
+    const inputRef = useRef();
+
     const [data, setData] = useState([
-		{ id: 1, name: "Apple", price: 0.99 },
-		{ id: 2, name: "Orange", price: 0.99 },
-		{ id: 3, name: "Milk", price: 0.99 },
+		{ id: 1, name: "Apple", done: true },
+		{ id: 2, name: "Orange", done: false },
+		{ id: 3, name: "Milk", done: false },
 	]);
 
-    const add = () => {
-        setData([ ...data, {id: 4, name: 'Butter', price: 1.99} ]);
+    const remove = id => {
+        setData( data.filter(item => item.id !== id) );
     }
 
-    return <div>
-        <h1>Todo</h1>
-        <ul>
-            {data.map(item => {
-                return <Item key={item.id} item={item} />
-            })}
-        </ul>
-        <button onClick={add}>Add</button>
-    </div>
+    const toggle = id => {
+        setData(
+            data.map(item => {
+                if(item.id === id) item.done = !item.done;
+                return item;
+            })
+        );
+    }
+
+    const add = name => {
+        const id = data[data.length - 1].id + 1;
+        setData([ ...data, { id, name, done: false } ]);
+    }
+
+    return (
+		<div>
+			<h1>Todo ({ data.filter(item => !item.done).length })</h1>
+			<form
+				onSubmit={e => {
+					e.preventDefault();
+
+					const name = inputRef.current.value;
+					if (!name) return false;
+
+					add(name);
+
+					inputRef.current.value = "";
+					inputRef.current.focus();
+				}}>
+				<input
+					type="text"
+					ref={inputRef}
+				/>
+				<button>Add</button>
+			</form>
+			<ul>
+				{data
+					.filter(item => !item.done)
+					.map(item => {
+						return (
+							<Item
+								key={item.id}
+								item={item}
+								remove={remove}
+                                toggle={toggle}
+							/>
+						);
+					})}
+			</ul>
+			<hr />
+			<ul>
+				{data
+					.filter(item => item.done)
+					.map(item => {
+						return (
+							<Item
+								key={item.id}
+								item={item}
+								remove={remove}
+                                toggle={toggle}
+							/>
+						);
+					})}
+			</ul>
+		</div>
+	);
 }
--- todo-react/src/Item.jsx
+++ todo-react/src/Item.jsx
@@ -1,7 +1,14 @@
-export default function Item({ item }) {
+export default function Item({ item, remove, toggle }) {
 	return (
 		<li>
-			{item.name} ({item.price})
+			{item.done ? (
+				<button onClick={() => toggle(item.id)}>Undo</button>
+			) : (
+				<button onClick={() => toggle(item.id)}>Done</button>
+			)}
+            
+			{item.name}
+			<button onClick={() => remove(item.id)}>Del</button>
 		</li>
 	);
 }
