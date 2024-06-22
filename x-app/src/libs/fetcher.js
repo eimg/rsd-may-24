4d42b44003e5281a7eee7a0f17bbbc6c4d37d1b7
Ei Maung <eimg@fairwayweb.com>
2024-06-22 15:11:48 +0630

    login

--- x-app/src/libs/fetcher.js
+++ x-app/src/libs/fetcher.js
@@ -1,5 +1,38 @@
 const api = import.meta.env.VITE_API;
 
+export async function fetchVerify() {
+    const token = localStorage.getItem("token");
+
+    const res = await fetch(`${api}/users/verify`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    });
+
+    if(res.ok) {
+        return await res.json();
+    }
+
+    return false;
+}
+
+export async function postLogin(username, password) {
+    const res = await fetch(`${api}/users/login`, {
+		method: "POST",
+		body: JSON.stringify({ username, password }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+    if(res.ok) {
+        const data = await res.json();
+        return data.token;
+    }
+
+	return false;
+}
+
 export async function getPosts() {
     const res = await fetch(`${api}/posts`);
 
--- x-app/src/pages/Login.jsx
+++ x-app/src/pages/Login.jsx
@@ -1,23 +1,61 @@
-import { Box, Button, OutlinedInput, Typography } from "@mui/material";
+import { Box, Button, OutlinedInput, Typography, Alert } from "@mui/material";
+
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useApp } from "../ThemedApp";
+import { fetchVerify, postLogin } from "../libs/fetcher";
 
 export default function Login() {
+    const navigate = useNavigate();
+    const { setAuth } = useApp();
+
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+
+    const [hasError, setHasError] = useState("");
+
     return (
 		<Box sx={{ textAlign: "center" }}>
 			<Typography variant="h3" sx={{ mb: 3 }}>Login</Typography>
-			<form>
+
+            {hasError && <Alert severity="warning" sx={{ mb: 4 }}>{hasError}</Alert>}
+
+			<form onSubmit={e => {
+                e.preventDefault();
+
+                const username = usernameRef.current.value;
+                const password = passwordRef.current.value;
+                if(!username || !password) {
+                    setHasError("Username and Password required");
+                    return false;
+                }
+
+                (async () => {
+                    const token = await postLogin(username, password);
+                    if(token) {
+                        localStorage.setItem("token", token);
+                        setAuth(await fetchVerify());
+                        navigate("/");
+                    } else {
+                        setHasError("Incorrect username or password");
+                    }
+                })();
+            }}>
 				<OutlinedInput
+                    inputRef={usernameRef}
 					fullWidth
 					placeholder="Username"
 					sx={{ mb: 2 }}
 				/>
 				<OutlinedInput
+                    inputRef={passwordRef}
 					fullWidth
 					type="password"
 					placeholder="Password"
 					sx={{ mb: 2 }}
 				/>
 
-				<Button fullWidth variant="contained">Login</Button>
+				<Button type="submit" fullWidth variant="contained">Login</Button>
 			</form>
 		</Box>
 	);
